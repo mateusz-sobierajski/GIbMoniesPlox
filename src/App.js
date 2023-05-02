ca1c528099805b8eb2bc1342cbc4c746e9103fc3
Mateusz Sobierajski <mateusz.sobierajski.official@gmail.com>
2023-05-02 23:15:50 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <img src={"gamerz.jpg"} className="Matt" alt="matt" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Krzysiu lubi bananki
         </p>
         <a
           className="App-link"
